f2c72cf574422baf02e31d1559e72aa5ed364847
1106-Evans-Cade <cadeevans@nevada.unr.edu>
2024-05-06 19:45:17 +0000

    Name

--- images.c
+++ images.c
@@ -1,4 +1,4 @@
-//Authors: Jack Donelson, [Partner 2]
+//Authors: Jack Donelson, [Partner 2], Cade Evans, [Partner 1] 
 //Purpose: User can load, display or edit pre-stored image file and save it as another image.
 #include <stdio.h>
 #include <stdbool.h>
