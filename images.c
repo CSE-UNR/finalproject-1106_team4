c48faf116e97ad45a2dd582fa4889dad963d1f1b
1106-Donelson-Jack <jack.donelson7@gmail.com>
2024-05-06 18:38:38 +0000

    Big chunk

--- images.c
+++ images.c
@@ -3,14 +3,70 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define MACRO 50
+#define MAX_SIZE 10000
+
+int width, height;
+int pixels[MAX_SIZE][MAX_SIZE]
+
+void loadImg(char *fileName);
+void convertImg();
+void displayImage();
+void editMenu();
+void cropImg();
+void dimImg();
+void brightenImg();
+void saveImg(char *fileName);
+
+
+
 
 //explicit *pointer
 int main() {
 
+	char fileName[100];
+
+	int selection;
+	
+	while(true){
+
+	printf(" Menu: \n");
+	printf("1. Load the Image: \n");
+	printf("2. Display the Image: \n");
+	printf("3. Edit the Image: \n");
+	printf("4. Exit: \n");
+	printf("Enter your choice: \n");
+	scanf("%d", &selection);
+	
+	switch (selection){
+	
+		case 1:
+		printf("Enter your file name: ");
+		scanf("%c", fileName);
+		loadImg(fileName);
+		break;
+		
+		case 2:
+		displayMenu();
+		break;
+		
+		case 3:
+		editMenu();
+		break;
+		
+		case 4:
+		
+		return 0;
+		break;
+		
+		default:
+		printf("Invalid selection. Please try again.\n");
+		break;
+		}
+	}					
+
 }
 
-int loadImg() {
+int loadImg(Image *image, char *fileName) {
     
 }
 
@@ -24,16 +80,92 @@ void displayImage() {
 
 int editMenu() {
 
+
+	char fileName[100];
+
+	int selection;
+	
+	while(true){
+
+	printf(" Menu: \n");
+	printf("1. Crop Image: \n");
+	printf("2. Dim the Image: \n");
+	printf("3. Brighten the Image: \n");
+	printf("4. Save Image: \n");
+	printf("5. Back to main menu: \n");
+	printf("Enter your choice: \n");
+	scanf("%d", &selection);
+	
+	switch (selection){
+	
+		case 1:
+		cropImg();
+		break;
+		
+		case 2:
+		dimImg();
+		break;
+		
+		case 3:
+		brightenImg();
+		break;
+		
+		case 4:
+		saveImg(fileName);
+		break;
+		
+		case 5:
+		saveImg(fileName);
+		break;
+		
+		
+		default:
+		printf("Invalid selection. Please try again.\n");
+		break;
+		}
+	}					
+
+}
+
 }
 
 void cropImg(){
-
+	for(int j = 0; j < height; j++){
+	   for(int k = 0; k < width; k++){
+	     if(pixels[j][k] > 0){
+		pixels[j][k]--;
+			}
+		}
+	}
+	
 }
 
 void dimImg(){
+for(int j = 0; j < height; j++){
+	   for(int k = 0; k < width; k++){
+	     if(pixels[j][k] > 0){
+		pixels[j][k]--;
+			}
+		}
+	}
+	
+}
 
 }
 
 void brightenImg(){
+for(int j = 0; j < height; j++){
+	   for(int k = 0; k < width; k++){
+	     if(pixels[j][k] < 255){
+		pixels[j][k]++;
+			}
+		}
+	}
+	
+}
+
+}
+
+void saveImg(Image *image, char *fileName){
 
 }
