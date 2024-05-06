b0e22f30d869c807045905eac72d980e8746df7d
1106-Donelson-Jack <jack.donelson7@gmail.com>
2024-05-06 07:44:04 +0000

    Images.c

---
+++ images.c
@@ -0,0 +1,39 @@
+//Authors: Jack Donelson, [Partner 2]
+//Purpose: User can load, display or edit pre-stored image file and save it as another image.
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MACRO 50
+
+//explicit *pointer
+int main() {
+
+}
+
+int loadImg() {
+    
+}
+
+char convertImg() {
+
+}
+
+void displayImage() {
+
+}
+
+int editMenu() {
+
+}
+
+void cropImg(){
+
+}
+
+void dimImg(){
+
+}
+
+void brightenImg(){
+
+}
