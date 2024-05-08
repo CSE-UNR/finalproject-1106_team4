879c758cc7075fd7e72d552fc905953a4245af8b
1106-Evans-Cade <cadeevans@nevada.unr.edu>
2024-05-08 04:27:52 +0000

    Work so Far

--- images.c
+++ images.c
@@ -3,18 +3,17 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define MAX_SIZE 10000
+#define MAX_SIZE 3
 
-int width, height;
-int pixels[MAX_SIZE][MAX_SIZE];
-
-int loadImg(char *fileName);
-void displayImage(char img[][100]);
-void editMenu();
-void cropImg();
-void dimImg();
-void brightenImg();
-int saveImg(char *fileName);
+int row(char *fileName);
+int column(char *fileName);
+int loadImg(char *fileName, int pixels[MAX_SIZE][MAX_SIZE]);
+void displayImage(int pixels[MAX_SIZE][MAX_SIZE], char img[MAX_SIZE][MAX_SIZE], int row, int col);
+int editMenu(int pixels[][MAX_SIZE]);
+void cropImg(int pixels[][MAX_SIZE]);
+void dimImg(int pixels[][MAX_SIZE]);
+void brightenImg(int pixels[][MAX_SIZE]);
+int saveImg(char *fileName, int pixels[][MAX_SIZE]);
 
 
 
@@ -23,9 +22,13 @@ int saveImg(char *fileName);
 int main() {
 
 	char fileName[100];
-	char img[100][100];
+	char img[MAX_SIZE][MAX_SIZE];
+	int pixels[MAX_SIZE][MAX_SIZE];
 
 	int selection;
+	
+	int columns = 0;
+	int rows = 0;
 	
 	while(true){
 
@@ -34,8 +37,9 @@ int main() {
 	printf("2. Display the Image: \n");
 	printf("3. Edit the Image: \n");
 	printf("4. Exit: \n");
-	printf("Enter your choice: \n");
+	printf("Enter your choice: ");
 	scanf("%d", &selection);
+	printf("\n");
 	
 	switch (selection){
 	
@@ -45,16 +49,20 @@ int main() {
 		fgets(fileName, 100, stdin);
 
 		scanf("%s", fileName);
-
-		loadImg(fileName);
+		
+		columns = column(fileName);
+		rows = row(fileName);
+		printf("%d", rows);
+		printf("%d", columns);
+		loadImg(fileName, pixels);
 		break;
 		
 		case 2:
-		displayImage(img[100][100]);
+		displayImage(pixels, img, rows, columns);
 		break;
 		
 		case 3:
-		editMenu();
+		editMenu(pixels);
 		break;
 		
 		case 4:
@@ -70,36 +78,105 @@ int main() {
 
 }
 
-int loadImg(char *fileName) {
+int row(char *fileName){
+	int column = 0;
+	int r = 0;
+	int c = 0;
+	char temp;
+	
 	FILE *image = fopen(fileName, "r");
 	if (image == NULL){
-	printf("ERROR: Unable to open file for saving.\n");
-	return 0;
+		printf("ERROR: Unable to open file for saving.\n");
+		return 0;
 	}
-	fscanf(image, "%d %d", &width, &height);
-	for (int j = 0; j < width; j++){
-		for(int k = 0; k <height; k++){
-		fscanf(image, "%d", &pixels[j][k]);	
-		}
-	}
-	fclose(image);
-}
-void displayImage(char img[][100]) {
-
-	char can;
-	
-	for(int i = 0; i < 1000; i++){
-		for(int k = 0; can != 1 ; k++){
-			if(img[i][k] == "\n"){
-				can = 1;
-			} else {
-				printf("%c", img[i][k]);
+	while(fscanf(image, "%c", &temp) == 1){
+			if(temp == '\n'){
+				r++;
+				column = c;
+				c = 0;
+				
+			} else if(temp != '\n'){
+				c++;
 			}
 		}
-	}
+	fclose(image);
+	return r;
 }
 
-void editMenu(){
+int column(char *fileName){
+	int column = 0;
+	int r = 0;
+	int c = 0;
+	char temp;
+	
+	FILE *image = fopen(fileName, "r");
+	if (image == NULL){
+		printf("ERROR: Unable to open file for saving.\n");
+		return 0;
+	}
+	while(fscanf(image, "%c", &temp) == 1){
+			if(temp == '\n'){
+				r++;
+				column = c;
+				c = 0;
+				
+			} else if(temp != '\n'){
+				c++;
+			}
+		}
+	fclose(image);
+	return column;
+}
+
+int loadImg(char *fileName, int pixels[MAX_SIZE][MAX_SIZE]) {
+	int r = 0;
+	int c = 0;
+	char temp;
+	FILE *image = fopen(fileName, "r");
+	if (image == NULL){
+		printf("ERROR: Unable to open file for saving.\n");
+	return 0;
+	}
+		while(fscanf(image, "%c", &temp) == 1){
+			if(temp == '\n'){
+				r++;
+				c = 0;
+				
+			} else if(temp != '\n'){
+				fscanf(image, "%d", &pixels[r][c]);
+				c++;
+			}
+		}
+	fclose(image);
+}
+
+
+void displayImage(int pixels[MAX_SIZE][MAX_SIZE], char img[MAX_SIZE][MAX_SIZE], int row, int col) {
+	
+	for(int i = 0; i < row; i++){
+		for(int k = 0; k < col ; k++){
+				if(pixels[i][k] == 0){
+					img[i][k] = 'p';
+					printf("%c", img[i][k]);
+				} else if(pixels[i][k] == 1){
+					img[i][k] = '.';
+					printf("%c", img[i][k]);
+				} else if (pixels[i][k] == 2){
+					img[i][k] = 'o';
+					printf("%c", img[i][k]);
+				} else if (pixels[i][k] == 3){
+					img[i][k] = 'O';
+					printf("%c", img[i][k]);
+				} else if (pixels[i][k] == 4){
+					img[i][k] = '0';
+					printf("%c", img[i][k]);
+				}
+		}
+	}
+	
+}
+
+int editMenu(int pixels[][MAX_SIZE]){
 
 
 	char fileName[100];
@@ -120,22 +197,23 @@ void editMenu(){
 	switch (selection){
 	
 		case 1:
-		cropImg();
+		cropImg(pixels);
 		break;
 		
 		case 2:
-		dimImg();
+		dimImg(pixels);
 		break;
 		
 		case 3:
-		brightenImg();
+		brightenImg(pixels);
 		break;
 		
 		case 4:
-		saveImg("edited_Image.txt");
+		saveImg("edited_Image.txt", pixels);
 		break;
 		
 		case 5:
+		return 0;
 		break;
 		
 		
@@ -147,9 +225,9 @@ void editMenu(){
 
 }
 
-void cropImg(){
-	for(int j = 0; j < height; j++){
-	   for(int k = 0; k < width; k++){
+void cropImg(int pixels[][MAX_SIZE]){
+	for(int j = 0; j < MAX_SIZE; j++){
+	   for(int k = 0; k < MAX_SIZE; k++){
 	     if(pixels[j][k] > 0){
 		pixels[j][k]--;
 			}
@@ -158,20 +236,20 @@ void cropImg(){
 	
 }
 
-void dimImg(){
-	for(int j = 0; j < height; j++){
-	   for(int k = 0; k < width; k++){
+void dimImg(int pixels[][MAX_SIZE]){
+	for(int j = 0; j < MAX_SIZE; j++){
+	   for(int k = 0; k < MAX_SIZE ; k++){
 	     if(pixels[j][k] > 0){
 		pixels[j][k]--;
 			}
 		}
 	}
 	
-	}
+}
 
-void brightenImg(){
-	for(int j = 0; j < height; j++){
-	   for(int k = 0; k < width; k++){
+void brightenImg(int pixels[][MAX_SIZE]){
+	for(int j = 0; j < MAX_SIZE; j++){
+	   for(int k = 0; k < MAX_SIZE; k++){
 	     if(pixels[j][k] < 4){
 		pixels[j][k]++;
 			}
@@ -181,16 +259,15 @@ void brightenImg(){
 }
 
 
-int saveImg(char *fileName){
+int saveImg(char *fileName, int pixels[][MAX_SIZE]){
 
 	FILE *image = fopen(fileName, "w");
 	if (image == NULL){
 	printf("ERROR: Unable to open file for saving.\n");
 	return 0;
 	}
-	fscanf(image, "%d %d", &width, &height);
-	for (int j = 0; j < width; j++){
-		for(int k = 0; k <height; k++){
+	for (int j = 0; j < MAX_SIZE; j++){
+		for(int k = 0; k < MAX_SIZE; k++){
 		fprintf(image, "%d", pixels[j][k]);	
 		}
 		fprintf(image, "\n");
